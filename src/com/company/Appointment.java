1caeec4a5edd5c25ac9da8d5ed23d4d8487ab561
BT_1N3_30@bktech.local <BT_1N3_30@bktech.local>
2018-12-17 11:10:51 -0500

    Initial commit

---
+++ src/com/company/Appointment.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class Appointment {
+
+    public TimeInterval getTime()
+
+
+    public boolean conflictsWith(Appointment other)
+    {
+        boolean conflict = false;
+        if (other == )
+        {
+            conflict = true;
+        }
+        return conflict;
+    }
+
+}
